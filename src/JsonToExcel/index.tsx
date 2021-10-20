8fd5391075558917a2dc52bb1163498688849361
aalsinan <aalsinan@ejada.com>
2021-10-20 12:22:33 +0300

    minor

--- src/JsonToExcel/index.tsx
+++ src/JsonToExcel/index.tsx
@@ -8,7 +8,13 @@ const JSToExcel = () => {
         The purpose of this script is to take JS Objects (en,ar) and it will
         generate new excel sheet.
       </p>
-      <i>supports up to 2 levels of nesting</i>
+      <ul>
+        <li>Supports Up to 2 levels of nesting</li>
+        <li>
+          The reference is EN keys, i.e we will not add keys exist in Arabic but
+          not in English in English
+        </li>
+      </ul>
       <Step1 />
     </>
   );
